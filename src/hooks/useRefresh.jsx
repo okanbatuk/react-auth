3c100782c9e7877d4a82a7327d7588bf7802060d
myrn <okanbatuk@protonmail.com>
2023-06-26 23:30:52 +0300

    Changed refresh route

--- src/hooks/useRefresh.jsx
+++ src/hooks/useRefresh.jsx
@@ -2,15 +2,22 @@ import api from "../libs/api";
 import useAuth from "./useAuth";
 
 export default () => {
-  const { auth, setAuth } = useAuth();
+  const { setAuth } = useAuth();
 
   const refresh = async () => {
-    const { data } = await api(`/refresh/${auth.uuid}`, {
+    const { data } = await api(`/refresh`, {
       withCredentials: true,
     });
-    const { accessToken } = data.data;
-    setAuth((prev) => {
-      return { ...prev, accessToken: accessToken };
+    const { user, accessToken } = data.data;
+    setAuth({
+      user: {
+        uuid: user.uuid,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        roles: user.role,
+      },
+      accessToken: accessToken,
     });
     return accessToken;
   };
