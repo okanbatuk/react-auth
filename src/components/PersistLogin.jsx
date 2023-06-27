cf56d269a67da4e4a8e3059647bd394f88566da4
myrn <okanbatuk@protonmail.com>
2023-06-27 22:27:55 +0300

    Changed session management strategy

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -2,22 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import useRefresh from "../hooks/useRefresh";
+import useLogout from "../hooks/useLogout";
 
 export default () => {
   const { auth, persist } = useAuth();
   const refresh = useRefresh();
+  const logout = useLogout();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     let mounted = true;
-    const verifyToken = async () => {
-      try {
-        await refresh();
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    !auth?.accessToken && persist && verifyToken();
+
+    !auth?.accessToken &&
+      (async () => {
+        try {
+          /*
+           * If persist is true regenerate the token
+           * Otherwise remove the refresh token in cookies and backend
+           *
+           * */
+          persist ? await refresh() : await logout();
+        } catch (err) {
+          console.error(err);
+        }
+      })();
 
     mounted &&
       setTimeout(() => {
