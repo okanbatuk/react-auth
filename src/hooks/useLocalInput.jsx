7e63fc80b997abeb28635bec67c5f1f02f8cadc6
myrn <okanbatuk@protonmail.com>
2023-06-29 23:55:46 +0300

    Adding more functionality

--- src/hooks/useLocalInput.jsx
+++ src/hooks/useLocalInput.jsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 
+const getLocalValue = (key, initValue) => {
+  // Server side running
+  if (typeof window === "undefined") return initValue;
+
+  // if a value is already store
+  const localValue = JSON.parse(localStorage.getItem(key));
+  if (localValue) return localValue;
+
+  // return result of a function
+  return initValue instanceof Function ? initValue() : initValue;
+};
+
 export default (key, initValue) => {
-  const [value, setValue] = useState(
-    JSON.parse(localStorage.getItem(key)) || initValue
-  );
+  const [value, setValue] = useState(() => {
+    return getLocalValue(key, initValue);
+  });
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
